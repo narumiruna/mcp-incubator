24ca00ae114d6b14361c4a9268992d9ccb7c2df9
narumi <toucans-cutouts0f@icloud.com>
2025-03-31 23:28:27 +0800

    set default instructions

--- src/mcpservers/cli.py
+++ src/mcpservers/cli.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import Final
+
 import anyio
 import gradio as gr
 import typer
@@ -11,11 +13,14 @@ from dotenv import load_dotenv
 from .bot import Bot
 from .config import get_run_configs
 
+DEFAULT_INSTRUCTIONS: Final[str] = """使用台灣正體中文。擅長邏輯推理且謹慎，能夠將問題拆解並逐步進行思考。"""
+
+
 app = typer.Typer()
 
 
 @app.command()
-def bot(instructions: str | None = None) -> None:
+def bot() -> None:
     load_dotenv(find_dotenv())
 
     mcp_servers: list[MCPServer] = [
@@ -34,7 +39,7 @@ def bot(instructions: str | None = None) -> None:
         ),
     ]
 
-    bot = Bot(instructions=instructions, mcp_servers=mcp_servers)
+    bot = Bot(instructions=DEFAULT_INSTRUCTIONS, mcp_servers=mcp_servers)
 
     run_configs = get_run_configs()
     provider_names = list(run_configs.keys())
@@ -53,24 +58,10 @@ def bot(instructions: str | None = None) -> None:
                     allow_flagging="never",
                     clear_btn=None,
                 )
-                # switch model
-                # gr.Interface(
-                #     fn=bot.set_model,
-                #     inputs=[gr.Dropdown(choices=["gpt-4o", "gpt-4o-mini", "gemini-2.0-flash"], label="Model")],
-                #     outputs=[],
-                #     live=True,
-                #     allow_flagging="never",
-                #     clear_btn=None,
-                # )
             # set instructions
             gr.Interface(
                 fn=bot.set_instructions,
-                inputs=[
-                    gr.Textbox(
-                        label="Instructions",
-                        value="使用台灣正體中文。擅長邏輯思考且嚴謹，會分解問題並且一步一步地思考。",
-                    )
-                ],
+                inputs=[gr.Textbox(label="Instructions", value=bot.agent.instructions)],
                 outputs=[],
                 allow_flagging="never",
                 clear_btn=None,
